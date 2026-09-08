5d2445db64cf57bdffa40a425c2e54034d0445b1
amirriza-eft <nasser.man@gmail.com>
2026-09-08 16:28:05 +0330

    changing the database to class

--- src/config/database.php
+++ src/config/database.php
@@ -17,3 +17,22 @@ try {
 } catch(PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
+
+
+// class DB{
+//     private $pdo;
+
+//     const HOST = 'php81_dev_environment_database';
+//     $db = 'php81_dev_environment';
+//     $user = 'root';
+//     $password = '1234';
+
+//     public function __construct()
+//     {
+//         $this->pdo = new PDO(
+//         "mysql:host=".self::HOST.";dbname=".self::DB.";charset=utf8mb4",
+//         self::USER,
+//         self::PASSWORD
+//     );
+//     }
+// }
